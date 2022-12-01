9463e4a74050a18c4dff42dfddb1decb93f88aea
yannicktekulve <yannick.te.kulve@topicus.nl>
2022-12-01 09:39:51 +0100

    day 1

---
+++ pkg/day1/day1.go
@@ -0,0 +1,56 @@
+package day1
+
+import (
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func task1(input string) int {
+	var splitPerElf = strings.Split(input, "\n\n")
+	var highest int
+	for i := 0; i < len(splitPerElf); i++ {
+		var parsedCalories = []int{}
+		var caloriesPerElf = strings.Split(splitPerElf[i], "\n")
+		for _, calorie := range caloriesPerElf {
+			parsed, _ := strconv.Atoi(calorie)
+			parsedCalories = append(parsedCalories, parsed)
+		}
+		total := sum(parsedCalories)
+		if total > highest {
+			highest = total
+		}
+	}
+	return highest
+}
+
+func task2(input string) int {
+	var splitPerElf = strings.Split(input, "\n\n")
+	var highest = []int{0, 0, 0}
+	for i := 0; i < len(splitPerElf); i++ {
+		var parsedCalories = []int{}
+		var caloriesPerElf = strings.Split(splitPerElf[i], "\n")
+		for _, calorie := range caloriesPerElf {
+			parsed, _ := strconv.Atoi(calorie)
+			parsedCalories = append(parsedCalories, parsed)
+		}
+		total := sum(parsedCalories)
+		for i, value := range highest {
+
+			if value < total {
+				highest[i] = total
+				sort.Ints(highest)
+				break
+			}
+		}
+	}
+	return sum(highest)
+}
+
+func sum(x []int) int {
+	total := 0
+	for _, value := range x {
+		total += value
+	}
+	return total
+}
